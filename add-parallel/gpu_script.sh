b1a2638080f4b44143659a0123a6de7015713ee2
Oskar Weinfurtner <oskar.weinfurtner@icloud.com>
2017-08-01 12:05:14 +0200

    parallel adding

---
+++ add-parallel/gpu_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J GPU_student_job
+#SBATCH -p GPU
+##SBATCH -p Student_GPU
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+##Maximum runtime <= 72:00:00
+#SBATCH --time=00:05:00
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
+
+module load cuda/5.0
+
+./add-parallel
+
+exit
